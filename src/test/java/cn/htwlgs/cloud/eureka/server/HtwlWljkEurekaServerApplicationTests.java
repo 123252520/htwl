5d0698cf557a5ef9e94a00f96ef57be22dd38e16
Gogs <gogs@fake.local>
2018-06-29 09:59:02 +0800

    first

---
+++ src/test/java/cn/htwlgs/cloud/eureka/server/HtwlWljkEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.htwlgs.cloud.eureka.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HtwlWljkEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
